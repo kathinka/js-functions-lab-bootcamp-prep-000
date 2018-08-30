dbbd8e70a1500ccc66eff782ab928dd165873f69
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-08-30 20:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function happyHolidays(){
   return ("Happy holidays!");
 }
 
-function happyHolidaysTo(name){
-  return("Happy holidays, ${name}!");
+function happyHolidaysTo(holiday, name){
+  return("Happy ${holiday}, ${name}!");
 }
 
 function holidayCountdown (days, holiday){
